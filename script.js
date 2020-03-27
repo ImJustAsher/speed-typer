633d3234b77bcf3b4d1ce9ae6b1a81d547db9822
Asher <asher_3@hotmail.co.uk>
2020-03-27 14:25:05 +0000

    finished project

--- script.js
+++ script.js
@@ -1,63 +1,133 @@
-const word = document.getElementById('word')
-const text = document.getElementById('text')
-const scoreEl = document.getElementById('score')
-const timeEl = document.getElementById('time')
-const endgameEl = document.getElementById('end-game')
-const settingsBtn = document.getElementById('settings-btn')
-const settings = document.getElementById('settings')
-const settingsForm = document.getElementById('settings-form')
-const difficultySelect = document.getElementById('difficulty')
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.getElementById('end-game-container');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
 
 const words = [
-  'sigh',
-  'tense',
-  'airplane',
-  'ball',
-  'pies',
-  'juice',
-  'warlike',
-  'bad',
-  'north',
-  'dependent',
-  'steer',
-  'silver',
-  'highfalutin',
-  'superficial',
-  'quince',
-  'eight',
-  'feeble',
-  'admit',
-  'drag',
-  'loving'
-]
+	'sigh',
+	'tense',
+	'airplane',
+	'ball',
+	'pies',
+	'juice',
+	'warlike',
+	'bad',
+	'north',
+	'dependent',
+	'steer',
+	'silver',
+	'highfalutin',
+	'superficial',
+	'quince',
+	'eight',
+	'feeble',
+	'admit',
+	'drag',
+	'loving'
+];
 
 //Init word
-let randomWord
+let randomWord;
 
 //Init score
-let score = 0
+let score = 0;
 
 //Init time
-let time = 10
+let time = 10;
+
+// Set difficulty to value in localstorage or medium
+let difficulty =
+	localStorage.getItem('difficulty') !== null
+		? localStorage.getItem('difficulty')
+		: 'medium';
+
+//Set difficulty select value
+difficultySelect.value =
+	localStorage.getItem('difficulty') !== null
+		? localStorage.getItem('difficulty')
+		: 'medium';
+
+//Focus on text on start
+text.focus();
+
+//Start counting down
+const timeInterval = setInterval(updateTime, 1000);
 
 //Generate random word from array
-function getRandomWord () {
-  return words[Math.floor(Math.random() * words.length)]
+function getRandomWord() {
+	return words[Math.floor(Math.random() * words.length)];
 }
 
 //Add word to DOM
-function addWordToDOM () {
-  randomWord = getRandomWord()
-  word.innerHTML = randomWord
+function addWordToDOM() {
+	randomWord = getRandomWord();
+	word.innerHTML = randomWord;
 }
 
-addWordToDOM()
+//Update score
+function updateScore() {
+	score++;
+	scoreEl.innerHTML = score;
+}
 
-//Event listeners
-text.addEventListener('input', e => {
-  const insertedText = e.target.value
+//Update time
+function updateTime() {
+	time--;
+	timeEl.innerHTML = time + 's';
 
-  if (insertedText === randomWord) {
-    addWordToDOM()
-  }
-})
+	if (time === 0) {
+		clearInterval(time);
+		//end game
+		gameOver();
+	}
+}
+
+//Game over, show end screen
+function gameOver() {
+	endgameEl.innerHTML = `
+  <h1>Time ran out</h1>
+  <p>Your final score is ${score}</p>
+  <button onclick="location.reload()">Reload</button>
+  `;
+
+	endgameEl.style.display = 'flex';
+}
+
+addWordToDOM();
+
+// Typing
+text.addEventListener('input', (e) => {
+	const insertedText = e.target.value;
+
+	if (insertedText === randomWord) {
+		addWordToDOM();
+		updateScore();
+
+		//Clear
+		e.target.value = '';
+
+		if (difficulty === 'hard') {
+			time += 2;
+		} else if (difficulty === 'medium') {
+			time += 3;
+		} else {
+			time += 5;
+		}
+
+		updateTime();
+	}
+});
+
+//Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+//Settings select
+settingsForm.addEventListener('change', (e) => {
+	difficulty = e.target.value;
+	localStorage.setItem('difficulty', difficulty);
+});
